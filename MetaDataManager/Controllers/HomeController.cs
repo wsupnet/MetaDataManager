b22551ff8b484c0c23bbc145f8fd8f6191dabe13
Wes <Wes@DESKTOP-16T4F05>
2018-02-07 09:06:50 -0800

    Integrated Spotify Api into project Home page

--- MetaDataManager/Controllers/HomeController.cs
+++ MetaDataManager/Controllers/HomeController.cs
@@ -9,22 +9,63 @@ using System.Net;
 using System.Text;
 using System.IO;
 using System.Net.Http;
-using Newtonsoft.Json;
 using System.Net.Http.Headers;
+using System.Web.UI.WebControls.Expressions;
+
+using SpotifyAPI.Web; //Base Namespace
+using SpotifyAPI.Web.Auth; //All Authentication-related classes
+using SpotifyAPI.Web.Enums; //Enums
+using SpotifyAPI.Web.Models; //Models for the JSON-responses
+using Newtonsoft.Json;
 
 namespace MetaDataManager.Controllers
 {
     public class HomeController : Controller
     {
 
-        public ActionResult Index()
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //private static SpotifyWebAPI _spotify;
+        static ClientCredentialsAuth auth;
+        public async Task<ActionResult> Index()
         {
 
-            string trackTitle = "Back In Black";
-            string trackArtist = "ACDC";
-            string url = string.Format("https://api.spotify.com/v1/search?q={0} {1}&type=track&market=US&limit=10&offset=0", trackTitle, trackArtist);
+            //string trackTitle = "Back In Black";
+            //string trackArtist = "ACDC";
+            //string url = string.Format("https://api.spotify.com/v1/search?q={0} {1}&type=track&market=US&limit=10&offset=0", trackTitle, trackArtist);
 
-            var jsonData = GetTrackInfo(url);
+            //var jsonData = GetTrackInfo(url);
+
+            //Create the auth object
+            auth = new ClientCredentialsAuth()
+            {
+                //Your client Id
+                ClientId = "d465cd5175d04b038cca6f1679643396",
+                //Your client secret UNSECURE!!
+                ClientSecret = "b136e21e115b49b0bb6afd6f3560192e",
+                //How many permissions we need?
+                Scope = Scope.UserReadPrivate,
+            };
+            //With this token object, we now can make calls
+            Token token = auth.DoAuth();
+            var spotify = new SpotifyWebAPI()
+            {
+                TokenType = token.TokenType,
+                AccessToken = token.AccessToken,
+                UseAuth = true
+            };
+
+            //SearchItem track = spotify.SearchItems("roadhouse+blues", SearchType.Album | SearchType.Playlist);
+            //var track = spotify.SearchItems("roadhouse+blues", SearchType.Album | SearchType.Playlist);
+            var track = spotify.SearchItems("bts", SpotifyAPI.Web.Enums.SearchType.Artist | SpotifyAPI.Web.Enums.SearchType.Playlist);
+
+
+            ViewData["ArtistsJson"] = JsonConvert.SerializeObject(track.Artists);
+            ViewData["Artists"] = track.Artists.Items.ToList();
+
 
             return View();
         }
@@ -41,87 +82,6 @@ namespace MetaDataManager.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
-        }
-
-        public string GetAccessToken()
-        {
-            SpotifyToken token = new SpotifyToken();
-            string url5 = "https://accounts.spotify.com/api/token";
-
-
-            var encode_clientid_clientsecret = Convert.ToBase64String(Encoding.UTF8.GetBytes(string.Format("{0}:{1}", "d465cd5175d04b038cca6f1679643396", "b136e21e115b49b0bb6afd6f3560192e")));
-
-
-            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url5);
-
-
-            webRequest.Method = "POST";
-            webRequest.ContentType = "application/x-www-form-urlencoded";
-            webRequest.Accept = "application/json";
-            webRequest.Headers.Add("Authorization: Basic " + encode_clientid_clientsecret);
-
-
-            var request = ("grant_type=client_credentials");
-            byte[] req_bytes = Encoding.ASCII.GetBytes(request);
-            webRequest.ContentLength = req_bytes.Length;
-
-
-            Stream strm = webRequest.GetRequestStream();
-            strm.Write(req_bytes, 0, req_bytes.Length);
-            strm.Close();
-
-
-            HttpWebResponse resp = (HttpWebResponse)webRequest.GetResponse();
-            String json = "";
-            using (Stream respStr = resp.GetResponseStream())
-            {
-                using (StreamReader rdr = new StreamReader(respStr, Encoding.UTF8))
-                {
-                    //should get back a string i can then turn to json and parse for accesstoken
-                    json = rdr.ReadToEnd();
-                    rdr.Close();
-                }
-            }
-            token = JsonConvert.DeserializeObject<SpotifyToken>(json);
-            return token.Access_token;
-        }
-
-        private string GetTrackInfo(string url)
-        {
-            string webResponse = string.Empty;
-            var myToken = GetAccessToken();
-            try
-            {
-                // Get token for request
-                HttpClient hc = new HttpClient();
-                var request = new HttpRequestMessage()
-                {
-                    RequestUri = new Uri(url),
-                    Method = HttpMethod.Get
-                };
-                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                request.Headers.Authorization = new AuthenticationHeaderValue("Authorization", "Bearer " + myToken);
-                // TODO: Had a task cancelled here, too many errors. Find a better way to process this maybe.
-                // Same code is in GetTrackFeatures function.
-                var task = hc.SendAsync(request)
-                    .ContinueWith((taskwithmsg) => {
-                        var response = taskwithmsg.Result;
-                        var jsonTask = response.Content.ReadAsStringAsync();
-                        webResponse = jsonTask.Result;
-                    });
-                task.Wait();
-
-
-            }
-            catch (WebException ex)
-            {
-                Console.WriteLine("Track Request Error: " + ex.Status);
-            }
-            catch (TaskCanceledException tex)
-            {
-                Console.WriteLine("Track Request Error: " + tex.Message);
-            }
-            return webResponse;
         }
     }
 }
